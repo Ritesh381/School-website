c99f3f74027f49f5f1de633ac5d18d3605fe6b35
Ritesh Prajapati <riteshprajapati@Riteshs-MacBook-Air.local>
2025-07-19 18:21:55 +0530

    Changes in contact form

--- src/components/ContactUsSection.jsx
+++ src/components/ContactUsSection.jsx
@@ -107,10 +107,10 @@ const ContactUsSection = () => {
                   htmlFor="email"
                   className="block text-sm font-medium text-gray-700 mb-1"
                 >
-                  Email
+                  Email/Phone
                 </label>
                 <input
-                  type="email"
+                  type="text"
                   id="email"
                   name="email"
                   className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
